080ae520046b0d66941cbbf092ad254193806277
Howon Lee <howon@howonlee.com>
2015-01-20 23:14:48 -0800

    we see a pattern of sparsification

--- symbols.py
+++ symbols.py
@@ -1,17 +1,33 @@
 import string
 import itertools
+import numpy as np
+import matplotlib.pyplot as plt
 
 def test(seqs):
     pos = 0
+    pos_seqs = []
     neg = 0
+    neg_seqs = []
     for alpha in seqs:
         if alpha in corpus:
             pos += 1
+            pos_seqs.append(alpha)
         else:
             neg += 1
+            neg_seqs.append(alpha)
     print "positive: ", pos
     print "negative: ", neg
-    return pos, neg
+    return pos_seqs, neg_seqs
+
+def ctoi(char):
+    return ord(char) - 97
+
+def display(seqs):
+    seq_mat = np.zeros((26,26))
+    for alpha in seqs:
+        seq_mat[ctoi(alpha[0]), ctoi(alpha[1])] += 1
+    plt.matshow(seq_mat)
+    plt.show()
 
 if __name__ == "__main__":
     corpus = ""
@@ -21,5 +37,5 @@ if __name__ == "__main__":
     order2 = map("".join, itertools.product(order1, repeat=2))
     order3 = map("".join, itertools.product(order1, repeat=3))
     #test(order1)
-    #test(order2)
-    test(order3)
+    pos, neg = test(order2)
+    display(neg)
